fc7bfa60757aa1430ed1c02855b9a86d1e91dabe
seasidesparrow <matthew.templeton@cfa.harvard.edu>
2018-01-23 09:14:56 -0500

    modified:   pyingest/__init__.py  	modified:   setup.py

--- setup.py
+++ setup.py
@@ -3,5 +3,6 @@ from setuptools import setup
 setup(
     name='pyingest',
     version='0.5.0',
-    packages=['pyingest']
+    packages=['pyingest'],
+    package_dir={'':['config','extractors','parsers','serializers','validators']}
 )
