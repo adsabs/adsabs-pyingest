4368eb04cebc1d4e1c610c02ba0c7bcb55219f76
seasidesparrow <matthew.templeton@cfa.harvard.edu>
2018-07-02 12:21:25 -0400

    modified:   pyingest/parsers/procsci.py

--- pyingest/parsers/procsci.py
+++ pyingest/parsers/procsci.py
@@ -4,6 +4,15 @@ from argparse import ArgumentParser
 import urllib
 from default import BaseBeautifulSoupParser
 
+class URLError(Exception):
+    pass
+
+class ContentError(Exception):
+    pass
+
+class NotPublishedError(Exception):
+    pass
+
 class PoSParser(BaseBeautifulSoupParser):
 
 # PoSParser will return a list of articles taken from a Proceedings of Science
@@ -15,7 +24,13 @@ class PoSParser(BaseBeautifulSoupParser):
         pass
 
     def get_buffer(self, url, **kwargs):
-        buff = urllib.urlopen(url)
+        hostname = url.split('/')[2]
+        if hostname != 'pos.sissa.it':
+            raise URLError("This parser is only for pos.sissa.it.")
+        try:
+            buff = urllib.urlopen(url)
+        except IOError:
+            raise URLError("Could not open from %s"%url)
         return buff
 
     def resource_dict(self, fp, **kwargs):
@@ -28,58 +43,66 @@ class PoSParser(BaseBeautifulSoupParser):
         pos_toc = [{}]
 
         data = self.resource_dict(self.get_buffer(url))
-             
+
 
         # Title:
         title = data.head.title.get_text()
 
         body = data.body
 
-        # Abstract:
-        abstract = body.find("div", {"id":"abstract"}).get_text().replace('\r','').replace('\n','').lstrip().rstrip()
+        if isinstance(body,type(None)):
+            raise ContentError ("Data from %s is not formatted correctly, check URL."%url)
+        else:
+            text_message = body.find("div", {"id":"text_message"})
+            if not isinstance(text_message,type(None)):
+                raise NotPublishedError ("The desired volume is not published.")
+            else:
 
-        conf_info  = body.find_all("DIV")
-        dstring = "conference_date>"
-        for c in conf_info:
-            if dstring in c.get_text():
-                for x in c.find_all("BR"):
-                    x.replaceWith(". ")
-        # Pubdate:
-                pubdate = c.get_text().split()[2] + "/01"
-        # Publication:
-                publication = title + ". " + c.get_text().replace(dstring,'').replace(' .','.')
+                # Abstract:
+                abstract = body.find("div", {"id":"abstract"}).get_text().replace('\r','').replace('\n','').lstrip().rstrip()
 
-        pos_toc.append({'title':title,'abstract':abstract,'pubdate':pubdate,'publication':publication})
+                conf_info  = body.find_all("DIV")
+                dstring = "conference_date>"
+                for c in conf_info:
+                    if dstring in c.get_text():
+                        for x in c.find_all("BR"):
+                            x.replaceWith(". ")
+                # Pubdate:
+                        pubdate = c.get_text().split()[2] + "/01"
+                # Publication:
+                        publication = title + ". " + c.get_text().replace(dstring,'').replace(' .','.')
+
+                pos_toc.append({'title':title,'abstract':abstract,'pubdate':pubdate,'publication':publication})
 
 
-        papers = body.find_all('td')
+                papers = body.find_all('td')
 
-        for p in papers:
-            article = {}
-            title = p.find("span", {"class":"contrib_title"})
-            pdf = p.find("span", {"class":"contrib_file"})
-            auths = p.find("span", {"class":"contrib_authors"})
-            page = p.find("span", {"class":"contrib_code"})
+                for p in papers:
+                    article = {}
+                    title = p.find("span", {"class":"contrib_title"})
+                    pdf = p.find("span", {"class":"contrib_file"})
+                    auths = p.find("span", {"class":"contrib_authors"})
+                    page = p.find("span", {"class":"contrib_code"})
 
-            # Title:
-            if not isinstance(title,type(None)):
-                article['title'] = title.get_text()
+                    # Title:
+                    if not isinstance(title,type(None)):
+                        article['title'] = title.get_text()
 
-            # Authors:
-            if not isinstance(auths,type(None)):
-                article['authors'] = [auths.get_text()]
+                    # Authors:
+                    if not isinstance(auths,type(None)):
+                        article['authors'] = [auths.get_text()]
+    
+                    # Page:
+                    if not isinstance(page,type(None)):
+                        article['page'] = str(int(page.a["href"].split('/')[2]))
 
-            # Page:
-            if not isinstance(page,type(None)):
-                article['page'] = str(int(page.a["href"].split('/')[2]))
+                    # Properties (PDF URL):
+                    if not isinstance(pdf,type(None)):
+                        if not isinstance(pdf.a,type(None)):
+                            url = "https://pos.sissa.it" + pdf.a['href']
+                            article['properties'] = {'PDF':url}
 
-            # Properties (PDF URL):
-            if not isinstance(pdf,type(None)):
-                if not isinstance(pdf.a,type(None)):
-                    url = "https://pos.sissa.it" + pdf.a['href']
-                    article['properties'] = {'PDF':url}
+                    if article != dict():
+                        pos_toc.append(article)
 
-            if article != dict():
-                pos_toc.append(article)
-
-        return pos_toc
+                return pos_toc
