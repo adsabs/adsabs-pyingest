7a4eb4aeaffbbf108dcd93fe4a902c120b06c15f
Edwin Henneken <ehenneken@gmail.com>
2019-07-30 12:12:51 -0400

    small update

--- test_hst.py
+++ test_hst.py
@@ -4,13 +4,11 @@ import sys
 from pyingest.parsers.hstprop import HSTParser
 from pyingest.serializers.classic import Tagged
 
-#try:
-#    token = os.environ['hst_token']
-#except KeyError
-#
-#sys.exit()
-#token = os.environ['hst_token']
-token = '44a742c5-dc5a-4a66-a6e0-6e59b43d208d'
+try:
+    token = os.environ['hst_token']
+except KeyError:
+    sys.exit("Please set API key in environment")
+
 api_url = 'https://proper.stsci.edu/proper/adsProposalSearch/query'
 
 parser = HSTParser()
@@ -18,6 +16,5 @@ documents = parser.parse(api_url, api_key=token, fromDate = '2019-01-01', maxRec
 outputfp = open('hstprop.tag','a')
 for d in documents:
     serializer = Tagged()
-    serializer.write(d)
-#    serializer.write(d,outputfp)
+    serializer.write(d,outputfp)
 outputfp.close()
