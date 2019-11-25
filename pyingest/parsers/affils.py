806402a8a667e89cdb685c07945b96765f52392b
seasidesparrow <matthew.templeton@cfa.harvard.edu>
2019-11-25 10:37:36 -0500

    modified:   affils.py

--- pyingest/parsers/affils.py
+++ pyingest/parsers/affils.py
@@ -10,10 +10,11 @@ from adsputils import u2asc
 
 class AffiliationParser(BaseBeautifulSoupParser):
     """
-    Affiliation string parser, to deal with email, orcid tags
+    Affiliation string parser, to deal with email, ORCID tags
     """
 
     def __init__(self, input_string):
+        self.original_string = input_string
         self.input_tagged = self.resource_dict(input_string)
         pass
 
@@ -52,33 +53,114 @@ class AffiliationParser(BaseBeautifulSoupParser):
         return email
 
 
+    def find_untagged_email(self):
+        email_split = self.original_string.split()
+        email = u''
+        email_list = []
+        for e in email_split:
+            at = e.split('@')
+            if len(at) > 1 and re.search('\w\.\w',at[1]):
+#               email = e
+                email_list.append(e)
+            email = ', '.join(email_list)
+        email = re.sub(';','',email)
+        return email
+
+
     def parse(self):
-        orcid = self.find_orcid_tag()
-        email = self.find_email_tag()
-        new_string = self.input_tagged.p.extract().text
-        new_string = re.sub(' ;','',new_string)
-        if email != u'': 
-            new_string = new_string + '; ' + unicode(email) 
-        if orcid != u'':
-            new_string = new_string + '; ' + unicode(orcid)
-        return new_string
+#       try:
+            orcid = self.find_orcid_tag()
+            email = self.find_email_tag()
+            new_string = self.input_tagged.p.extract().text
+            new_string = re.sub(' ;','',new_string)
+            if orcid != u'':
+                new_string = new_string + '; ' + unicode(orcid)
+            if email != u'': 
+                new_string = new_string + '; ' + unicode(email) 
+            else:
+                email = self.find_untagged_email()
+                new_string = re.sub(email,'',new_string)
+                new_string = new_string + '; ' + unicode(email) 
+            new_string = re.sub(' ;','',new_string)
+            return new_string
+#       except:
+#           return self.original_string
 
 
-#def __main__():
-#    aff = "Harvard-Smithsonian Center for Astrophysics; <EMail>matthew.templeton@cfa.harvard.edu</EMail>; <ID system='ORCiD'>0000-0000-1234-5678</ID>; State Key Laboratory for Stamp Collecting, Ministry of Silly Walks, University of Basketweaving" 
-#
-#    parser = AffiliationParser(aff)
-#    new_string = parser.parse()
-#
-#    print new_string
-#
-#    aff = "Harvard-Smithsonian Center for Astrophysics; <fnord>matthew.templeton@cfa.harvard.edu</fnord>; <orcid>0000-0000-1234-5678</orcid>; State Key Laboratory for Stamp Collecting, Ministry of Silly Walks, University of Basketweaving" 
-#
-#    parser = AffiliationParser(aff)
-#    new_string = parser.parse()
-#
-#    print new_string
-#
-#
-#if __name__ == '__main__':
-#    __main__()
+def __main__():
+
+
+
+    print "\nONE:"
+    aff = "Harvard-Smithsonian Center for Astrophysics; <EMail>matthew.templeton@cfa.harvard.edu</EMail>; <ID system='ORCiD'>0000-0000-1234-5678</ID>; State Key Laboratory for Stamp Collecting, Ministry of Silly Walks, University of Basketweaving" 
+
+    parser = AffiliationParser(aff)
+    new_string = parser.parse()
+
+    print "\n"
+    print new_string
+    print "----------------------\n"
+
+
+
+    print "\nTWO:"
+    aff = "Harvard-Smithsonian Center for Astrophysics; <EMAIL>matthew.templeton@cfa.harvard.edu</EMAIL>; <ORCID>0000-0000-1234-5678</ORCID>; State Key Laboratory for Stamp Collecting, Ministry of Silly Walks, University of Basketweaving" 
+
+    parser = AffiliationParser(aff)
+    new_string = parser.parse()
+
+    print "\n"
+    print new_string
+    print "----------------------\n"
+
+
+
+    print "\nTHREE:"
+    aff = "Harvard-Smithsonian Center for Astrophysics; matthew.templeton@cfa.harvard.edu; <ORCID>0000-0000-1234-5678</ORCID>; State Key Laboratory for Stamp Collecting, Ministry of Silly Walks, University of Basketweaving" 
+
+    parser = AffiliationParser(aff)
+    new_string = parser.parse()
+
+    print "\n"
+    print new_string
+    print "----------------------\n"
+
+
+
+    print "\nFOUR:"
+    aff = "CfA @ Harvard; matthew.templeton@cfa.harvard.edu; <ORCID>0000-0000-1234-5678</ORCID>; State Key Laboratory for Stamp Collecting, Ministry of Silly Walks, University of Basketweaving" 
+
+    parser = AffiliationParser(aff)
+    new_string = parser.parse()
+
+    print "\n"
+    print new_string
+    print "----------------------\n"
+
+
+
+    print "\nFIVE:"
+    aff = "CfA@Harvard; matthew.templeton@cfa.harvard.edu; <ORCID>0000-0000-1234-5678</ORCID>; State Key Laboratory for Stamp Collecting, Ministry of Silly Walks, University of Basketweaving" 
+
+    parser = AffiliationParser(aff)
+    new_string = parser.parse()
+
+    print "\n"
+    print new_string
+    print "----------------------\n"
+
+
+
+    print "\nSIX:"
+    aff = "lol butts; <ORCID>0000-0000-1234-5678</ORCID>; brandewijn@gmail.com; State Key Laboratory for Stamp Collecting, Ministry of Silly Walks, University of Basketweaving; CfA@Harvard; foo@bar.baz" 
+
+    parser = AffiliationParser(aff)
+    new_string = parser.parse()
+
+    print "\n"
+    print new_string
+    print "----------------------\n"
+
+
+if __name__ == '__main__':
+    __main__()
