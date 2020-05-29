8c16abc773d6b5c107e4c6baea5c055b23a47956
seasidesparrow <matthew.templeton@cfa.harvard.edu>
2020-05-28 20:27:30 -0400

    new file:   examples/ex_harvest_pnas.py  	modified:   pyingest/config/config.py  	new file:   pyingest/parsers/pnas.py  	modified:   requirements.txt

--- pyingest/config/config.py
+++ pyingest/config/config.py
@@ -106,7 +106,7 @@ UAT_URL = 'https://raw.githubusercontent.com/astrothesaurus/UAT/master/UAT.json'
 try:
     uat_request = requests.get(UAT_URL)
     UAT_ASTRO_KEYWORDS = list(find('name', uat_request.json()))
-    print("Info: loaded %s UAT keywords from github." % len(UAT_ASTRO_KEYWORDS))
+    # print("Info: loaded %s UAT keywords from github." % len(UAT_ASTRO_KEYWORDS))
 except Exception as e:
     print("Warning: could not load UAT from github!")
     UAT_ASTRO_KEYWORDS = []
