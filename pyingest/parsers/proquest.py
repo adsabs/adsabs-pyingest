16dc6c0af6f42d47bfee67c3d131d303f241ddea
Kelly Lockhart <2926089+kelockhart@users.noreply.github.com>
2020-10-28 11:41:15 -0400

    Fix pymarc import issue

--- pyingest/parsers/proquest.py
+++ pyingest/parsers/proquest.py
@@ -5,8 +5,13 @@ import sys
 if sys.version_info > (3,) or 'unittest' in sys.modules.keys():
     import pymarc
 else:
-    raise ImportError('Error: ProQuest parser requires an older version of pymarc that is only compatible with Python 2'
-                      ' and it needs to be manually installed. Run the following: pip install "pymarc<=2.9.1"')
+    import pkg_resources
+    if pkg_resources.get_distribution('pymarc').version > '2.9.1':
+        raise ImportError('Error: ProQuest parser requires an older version of pymarc that is only compatible with '
+                          'Python 2 and it needs to be manually installed. '
+                          'Run the following: pip install "pymarc<=2.9.1"')
+    else:
+        import pymarc
 from pyingest.config import config
 from pyingest.parsers.author_names import AuthorNames
 from .default import DefaultParser
