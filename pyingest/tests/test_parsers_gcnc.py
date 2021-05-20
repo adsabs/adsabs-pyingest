bf421afa8e92f9b80804e5739985c5d69f14e551
seasidesparrow <matthew.templeton@cfa.harvard.edu>
2021-05-20 12:18:10 -0400

    deleted:    pyingest/tests/test_parsers.py  	new file:   pyingest/tests/test_parsers_affiliation.py  	new file:   pyingest/tests/test_parsers_aps.py  	new file:   pyingest/tests/test_parsers_arxiv.py  	new file:   pyingest/tests/test_parsers_atel.py  	new file:   pyingest/tests/test_parsers_authors.py  	new file:   pyingest/tests/test_parsers_datacite.py  	new file:   pyingest/tests/test_parsers_feedback.py  	new file:   pyingest/tests/test_parsers_gcnc.py  	new file:   pyingest/tests/test_parsers_hstprop.py  	new file:   pyingest/tests/test_parsers_iop.py  	new file:   pyingest/tests/test_parsers_joss.py  	new file:   pyingest/tests/test_parsers_oup.py  	new file:   pyingest/tests/test_parsers_pnas.py  	new file:   pyingest/tests/test_parsers_procsci.py  	new file:   pyingest/tests/test_parsers_proquest.py  	new file:   pyingest/tests/test_parsers_zenodo.py

---
+++ pyingest/tests/test_parsers_gcnc.py
@@ -0,0 +1,69 @@
+"""
+Test parsers
+"""
+from __future__ import print_function
+
+import unittest
+import filecmp
+import sys
+import os
+import glob
+import json
+import shutil
+from mock import patch, Mock, mock_open
+
+from pyingest.parsers import aps
+from pyingest.parsers import arxiv
+from pyingest.parsers import atel
+from pyingest.parsers import datacite
+from pyingest.parsers import gcncirc
+from pyingest.parsers import hstprop
+from pyingest.parsers import iop
+from pyingest.parsers import joss
+from pyingest.parsers import oup
+from pyingest.parsers import pnas
+from pyingest.parsers import proquest
+from pyingest.parsers import procsci
+from pyingest.parsers import zenodo
+from pyingest.config import config
+from pyingest.parsers.author_names import AuthorNames
+from pyingest.parsers.affils import AffiliationParser
+from pyingest.parsers import adsfeedback
+
+from pyingest.serializers import classic
+
+if sys.version_info > (3,):
+    open_mode = 'rb'
+    open_mode_u = 'rb'
+else:
+    open_mode = 'r'
+    open_mode_u = 'rU'
+
+class MockResponse(object):
+
+    def __init__(self, resp_data):
+        self.resp_data = resp_data
+
+    def read(self):
+        return self.resp_data
+
+
+class TestGCNC(unittest.TestCase):
+
+    def setUp(self):
+        stubdata_dir = os.path.join(os.path.dirname(__file__), 'data/stubdata')
+        self.inputdir = os.path.join(stubdata_dir, 'input/gcncirc')
+
+    # Test 28
+    def test_gcn_parser(self):
+        test_infile = os.path.join(self.inputdir, '25321.gcn3')
+        with open(test_infile) as fp:
+            data = fp.read()
+            parser = gcncirc.GCNCParser(data)
+            test_data = parser.parse()
+            output_bibcode = '2019GCN.25321....1I'
+            output_authors = 'IceCube Collaboration'
+            output_pub = u'GRB Coordinates Network, Circular Service, No. 25321'
+            self.assertEqual(test_data['bibcode'], output_bibcode)
+            self.assertEqual(test_data['authors'], output_authors)
+            self.assertEqual(test_data['publication'], output_pub)
---
+++ pyingest/tests/test_parsers_oup.py
@@ -0,0 +1,99 @@
+"""
+Test parsers
+"""
+from __future__ import print_function
+
+import unittest
+import filecmp
+import sys
+import os
+import glob
+import json
+import shutil
+from mock import patch, Mock, mock_open
+
+from pyingest.parsers import aps
+from pyingest.parsers import arxiv
+from pyingest.parsers import atel
+from pyingest.parsers import datacite
+from pyingest.parsers import gcncirc
+from pyingest.parsers import hstprop
+from pyingest.parsers import iop
+from pyingest.parsers import joss
+from pyingest.parsers import oup
+from pyingest.parsers import pnas
+from pyingest.parsers import proquest
+from pyingest.parsers import procsci
+from pyingest.parsers import zenodo
+from pyingest.config import config
+from pyingest.parsers.author_names import AuthorNames
+from pyingest.parsers.affils import AffiliationParser
+from pyingest.parsers import adsfeedback
+
+from pyingest.serializers import classic
+
+if sys.version_info > (3,):
+    open_mode = 'rb'
+    open_mode_u = 'rb'
+else:
+    open_mode = 'r'
+    open_mode_u = 'rU'
+
+class MockResponse(object):
+
+    def __init__(self, resp_data):
+        self.resp_data = resp_data
+
+    def read(self):
+        return self.resp_data
+
+
+class TestOUP(unittest.TestCase):
+
+    def setUp(self):
+        stubdata_dir = os.path.join(os.path.dirname(__file__), 'data/stubdata/')
+        self.inputdocs = glob.glob(os.path.join(stubdata_dir, 'input/oup*'))
+        self.maxDiff = None
+        self.outputdir = os.path.join(stubdata_dir, 'parsed')
+        self.tmplogdir = os.path.join(os.path.dirname(__file__), 'data/oup_tempfiles/')
+        # sys.stderr.write("test cases are: {}\n".format(self.inputdocs))
+        self.FAKE_OUP_TMP_DIRS = {
+            'mnrasl': self.tmplogdir + 'MNRASL',
+            'mnras':  self.tmplogdir + 'MNRAS',
+            'pasj.':  self.tmplogdir + 'PASJ',
+            'geoji':  self.tmplogdir + 'GeoJI'
+        }
+
+    # Test 16
+    def test_oup_parser(self):
+        with patch.dict(config.OUP_TMP_DIRS, self.FAKE_OUP_TMP_DIRS, clear=True):
+            parser = oup.OUPJATSParser()
+            for file in self.inputdocs:
+                # this will raise exceptions if something is wrong
+                with open(file, open_mode_u) as fp:
+                    test_data = parser.parse(fp)
+                    self.assertIsNotNone(test_data, "%s: error reading doc" % file)
+                basefile = os.path.basename(file)
+           
+                target = os.path.join(self.outputdir, basefile + '.parsed')
+
+                # post-parsing cleanup:
+                for f in glob.glob(self.tmplogdir + 'backup/*'):
+                    fi = os.path.basename(f)
+                    fo = self.tmplogdir + fi
+                    try:
+                        shutil.copyfile(f, fo)
+                    except Exception as err:
+                        pass
+
+                # # save temporary copy of data structure
+                # target_saved = target + '.NEW'
+                # with open(target_saved, 'w') as fp:
+                    # json.dump(test_data, fp, sort_keys=True, indent=4)
+
+                with open(target, 'r') as fp:
+                    shouldbe = json.load(fp)
+                    self.assertDictEqual(shouldbe, test_data, "results differ from %s" % target)
+
+                
+        return
