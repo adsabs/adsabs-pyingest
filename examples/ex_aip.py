3b1bf0882b30ae0cb858db9261fd9a9e3adbc46a
seasidesparrow <matthew.templeton@cfa.harvard.edu>
2021-06-15 06:38:45 -0400

    modified:   examples/ex_aip.py  	modified:   pyingest/parsers/affils.py  	modified:   pyingest/parsers/jats.py  	new file:   pyingest/parsers/jats_contrib.py  	modified:   pyingest/tests/data/stubdata/parsed/oup_mnrasl_early_slaa054.xml.parsed  	modified:   pyingest/tests/test_parsers_iop.py  	modified:   pyingest/tests/test_parsers_oup.py

--- examples/ex_aip.py
+++ examples/ex_aip.py
@@ -7,7 +7,7 @@ import pyingest.config.config as config
 # infile = '/proj/ads/abstracts/data/AIP/AIP.test/RSI/v91/i5/054901_1/Markup/VOR_10.1063_5.0005676.xml'
 # infile = '/proj/ads/abstracts/data/AIP/JATS.0127/JCP/v154/i2/024904_1/Markup/VOR_10.1063_5.0033645.xml'
 # infile = '/proj/ads/abstracts/data/AIP/JATS.0127/AJP/v89/i2/210_1/Markup/VOR_10.1119_10.0002365.xml'
-infile = '/Users/mtempleton/VOR_10.1063_5.0052678.xml'
+infile = '/proj/ads/abstracts/data/AIP/JATS.0609.new/RSI/v92/i6/064704_1/Markup/VOR_10.1063_5.0044438.xml'
 
 
 with open(infile,'r') as fp:
@@ -15,11 +15,11 @@ with open(infile,'r') as fp:
     doc = lol.parse(fp)
     # print("Hi, ... %s" % doc)
     wut = Tagged()
-    with open('lollol.lol','a') as fo:
+    with open('test.tag','a') as fo:
         wut.write(doc,fo)
-    foo = ReferenceWriter()
-    foo.topdir = './'
+#   foo = ReferenceWriter()
+#   foo.topdir = './'
     # try:
-    foo.writeref(doc,'aip')
+#   foo.writeref(doc,'aip')
     # except Exception as err:
         # print("Error with writeref:", err)
--- pyingest/tests/test_parsers_iop.py
+++ pyingest/tests/test_parsers_iop.py
@@ -29,6 +29,7 @@ class TestIOP(unittest.TestCase):
     def setUp(self):
         stubdata_dir = os.path.join(os.path.dirname(__file__), 'data/stubdata/')
         self.inputdir = os.path.join(stubdata_dir, 'input')
+        self.maxDiff = None
 
     # Test 15
     def test_iop_parser(self):
@@ -37,6 +38,11 @@ class TestIOP(unittest.TestCase):
         with open(test_infile, open_mode_u) as fp:
             input_data = fp.read()
         test_data = parser.parse(input_data)
+        # save temporary copy of data structure
+        
+        target_saved = test_infile.replace('input','parsed') + 'parsed.NEW'
+        with open(target_saved, 'w') as fp:
+            json.dump(test_data, fp, sort_keys=True, indent=4)
         output_bibcode = '2019ApJ...882...74H'
         output_pub = u'The Astrophysical Journal, Volume 882, Issue 2, id.74, <NUMPAGES>13</NUMPAGES> pp.'
         output_aff = [u'Department of Physics, The George Washington University, 725 21st St. NW, Washington, DC 20052, USA; The George Washington Astronomy, Physics, and Statistics Institute of Sciences (APSIS), The George Washington University, Washington, DC 20052, USA; Space Sciences Laboratory, 7 Gauss Way, University of California, Berkeley, CA 94720-7450, USA; <id system="ORCID">0000-0002-8548-482X</id>; <email>jhare@berkeley.edu</email>', u'Department of Physics, The George Washington University, 725 21st St. NW, Washington, DC 20052, USA; The George Washington Astronomy, Physics, and Statistics Institute of Sciences (APSIS), The George Washington University, Washington, DC 20052, USA; <id system="ORCID">0000-0002-6447-4251</id>', u'Department of Astronomy & Astrophysics, Pennsylvania State University, 525 Davey Lab, University Park, PA 16802, USA; <id system="ORCID">0000-0002-7481-5259</id>', u'Department of Physics, The George Washington University, 725 21st St. NW, Washington, DC 20052, USA; The George Washington Astronomy, Physics, and Statistics Institute of Sciences (APSIS), The George Washington University, Washington, DC 20052, USA; <id system="ORCID">0000-0001-7833-1043</id>']
