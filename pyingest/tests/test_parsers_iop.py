0e0385548f26f7e55137b43cac05c54336d36cb0
seasidesparrow <matthew.templeton@cfa.harvard.edu>
2021-06-29 07:22:04 -0400

    modified:   pyingest/parsers/iop.py  	modified:   pyingest/parsers/jats.py  	new file:   pyingest/tests/data/stubdata/input/iop_aj_accentnames.xml  	modified:   pyingest/tests/test_parsers_iop.py

--- pyingest/tests/test_parsers_iop.py
+++ pyingest/tests/test_parsers_iop.py
@@ -10,6 +10,8 @@ import glob
 import json
 import shutil
 
+import namedentities
+
 from pyingest.parsers import iop
 from pyingest.config import config
 from pyingest.parsers.author_names import AuthorNames
@@ -49,4 +51,21 @@ class TestIOP(unittest.TestCase):
         self.assertEqual(test_data['bibcode'], output_bibcode)
         self.assertEqual(test_data['publication'], output_pub)
         self.assertEqual(test_data['affiliations'], output_aff)
+        return
+
+    # Test 15a
+    def test_iop_entities(self):
+        test_infile = os.path.join(self.inputdir, 'iop_aj_accentnames.xml')
+        parser = iop.IOPJATSParser()
+        with open(test_infile, open_mode_u) as fp:
+            input_data = fp.read()
+        # input_data = namedentities.unicode_entities(str(input_data))
+        # input_data = bytes(input_data, encoding='utf-8')
+        test_data = parser.parse(input_data)
+        output_title = 'Meteoroid Stream of Comet C/1961 T1 (Seki) and Its Relation to the December &#961;-Virginids and &#947;-Sagittariids'
+        output_authors = 'Neslu&#353;an, Lubo&#353;; Hajdukov&#225;, M&#225;ria'
+        output_affiliations = ['Astronomical Institute, Slovak Academy of Science, 05960 Tatransk&#225; Lomnica, Slovakia; <ID system="ORCID">0000-0001-9758-1144</ID> <EMAIL>ne@ta3.sk</EMAIL>', 'Astronomical Institute, Slovak Academy of Science, D&#250;bravsk&#225; cesta 9, 84504 Bratislava, Slovakia; <ID system="ORCID">0000-0002-7837-2627</ID>']
+        self.assertEqual(test_data['title'], output_title)
+        self.assertEqual(test_data['authors'], output_authors)
+        self.assertEqual(test_data['affiliations'], output_affiliations)
         return
