c5d1ece469749b6beac6490919ec9d09ad49914a
seasidesparrow <matthew.templeton@cfa.harvard.edu>
2021-07-16 10:21:44 -0400

    modified:   examples/ex_gcncirc.py  	new file:   pyingest/tests/data/stubdata/input/gcncirc/14500.gcn3  	modified:   pyingest/tests/test_parsers_gcnc.py

--- examples/ex_gcncirc.py
+++ examples/ex_gcncirc.py
@@ -2,51 +2,33 @@ from __future__ import print_function
 from glob import glob
 from pyingest.parsers.gcncirc import GCNCParser
 from pyingest.serializers.classic import Tagged
-from namedentities import *
+from namedentities import hex_entities
 
+basedir = './pyingest/tests/data/stubdata/input/gcncirc/'
+inputFileList = glob(basedir + '*.gcn3')
+outputTagFile = './output.tag'
 
 def main():
 
-    basedir = 'pyingest/tests/data/stubdata/input/'
-    flist = glob(basedir + '*.gcn3')
-    # flist = ['25548.gcn3','23456.gcn3','23457.gcn3','23458.gcn3','25321.gcn3','9999.gcn3','98765.gcn3']
+    with open(outputTagFile, 'w') as fo:
 
-    with open('output.tag', 'w') as fo:
-        for f in flist:
-            f2 = basedir + f
+        for f in inputFileList:
+            print(f)
             try:
-                with open(f2, 'rU') as fg:
-                    try:
-                        d = fg.read()
-                    except Exception as err:
-                        d = ''
-                        print(f2)
-                        print("couldnt read it:", err)
-                try:
-                    # d = namedentities.hex_entities(d)
-                    d = repr(hex_entities(d))
-                except Exception as err:
-                    d = ''
-                    print(f2)
-                    print("Couldnt convert to hex:", err)
+                with open(f, 'r') as fg:
+                    d = fg.read()
+            except Exception as err:
+                print(f)
+                print("couldnt read it:", err)
+            else:
                 try:
                     x = GCNCParser(d)
-                except Exception as err:
-                    print("failed at GCNCParser(d) step:", err)
-                try:
                     y = x.parse()
-                except Exception as err:
-                    print("failed at x.parse step:", err)
-                try:
                     serializer = Tagged()
                     serializer.write(y, fo)
                 except Exception as err:
-                    print(f2)
-                    print("Couldnt serialize it:", err)
-            except Exception as err:
-                print("Problem parsing %s" % f2)
-                print("Error: %s" % err)
-
+                    print(f)
+                    print("Couldnt parse it:", err)
 
 if __name__ == '__main__':
     main()
