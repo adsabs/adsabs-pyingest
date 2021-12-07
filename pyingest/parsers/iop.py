ad62a770be2e054457351adc39e4ecd29c7f7256
Carolyn Grant <cgrant@cfa.harvard.edu>
2021-12-07 16:55:22 -0500

    Update iop.py

--- pyingest/parsers/iop.py
+++ pyingest/parsers/iop.py
@@ -133,8 +133,8 @@ class IOPJATSParser(JATSParser):
             bibstem = j_bibstem.ljust(5, '.')
             logging.debug('bibstem',bibstem)
             logging.debug('j_bibstem',j_bibstem)
-            if bibstem == IOP_PUBLISHER_IDS['jcap']:
-            # if bibstem == u'JCAP':
+            # if bibstem == IOP_PUBLISHER_IDS['jcap']:
+            if bibstem == 'JCAP.':
                 volume = output_metadata['issue'].rjust(4, '.')
                 logging.debug('JCAP bibcode',bibstem)
             else:
