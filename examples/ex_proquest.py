d3fd2b1472ed7ad82ba20a382d6e527ba61af374
Matthew Templeton <matthew.templeton@cfa.harvard.edu>
2023-06-28 14:59:36 -0400

    fix: debugging proquest problems  	modified:   examples/ex_proquest.py

--- examples/ex_proquest.py
+++ examples/ex_proquest.py
@@ -1,4 +1,5 @@
 from __future__ import print_function
+import os
 from glob import glob
 from pyingest.parsers.proquest import ProQuestParser
 from pyingest.serializers.classic import Tagged
@@ -7,10 +8,13 @@ from namedentities import *
 
 def main():
 
-    marc_filename = 'SAO_NASA_Jul_2020.UNX'
+    marc_filename = '/Users/mtemple/Desktop/SAO_NASA_May_2023.UNX'
     oa_filename = marc_filename.replace('.UNX', '_OpenAccessTitles.csv')
     marcdata = open(marc_filename).read()
-    oadata = open(oa_filename).read()
+    if os.path.exists(oa_filename):
+        oadata = open(oa_filename).read()
+    else:
+        oadata = ''
     parser = ProQuestParser(marcdata, oadata)
     lol = parser.parse()
     print("%s records processed" % len(parser.results))
